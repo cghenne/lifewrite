5f90b550d4b16d024ac537eb642449431d8cc076
Cathleen Ghenne <cathleen@workangel.com>
2016-11-18 17:15:58 +0000

    back end add users end point

---
+++ server/src/api/lifeworks.js
@@ -0,0 +1,18 @@
+/* eslint no-use-before-define:0 */
+// const _ = require('lodash');
+// const config = require('../config');
+// const querystring = require('querystring');
+
+const lifeworks = {};
+
+lifeworks.getUsers = () => {
+  return fetch('https://api.test.lifeworks.com/user', {
+    method: 'GET',
+     headers: {
+       'Accept': 'application/vnd.wam-api-v1.3+json',
+       'Wam-Token': '9iuz5e740ed151473bf61350d236b33ecd735e740ed151473bf61350d236b33ecd73'
+     }
+  });
+};
+
+module.exports = lifeworks;
