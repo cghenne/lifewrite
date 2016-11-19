9b9ccd6f06ad98f5c5c60a66830e6648a56e51da
Tomasz Zubertowski <tomasz.zubertowski@workangel.com>
2016-11-19 00:21:08 +0000

    Repository added for conversation

--- server/src/api/conversation.js
+++ server/src/api/conversation.js
@@ -1,19 +1,12 @@
 const conversation = {};
 var Conversation = require('../models/Conversation.js');
+var ConversationRepository = require('../repositories/Conversation.js');
 
 conversation.createOrFetchConversation = (req, res, userName, targetUserName) => {
-  var conversation = Conversation.findOne({'users' : [userName, targetUserName]});
-  console.log(conversation);
+  var conversation = ConversationRepository.findOneByUserIds(userName, targetUserName);
   if (!conversation) {
-    var conversation = new Conversation({
-      name: userName + 'Conversation',
-      created_on: new Date(),
-      conversation_history: [],
-      users: [userName, targetUserName]
-    });
-    conversation.save(function (err) {
-      if(err) res.status(404).send(err);
-    });
+    ConversationRepository.create(undefined, userName, targetUserName);
+    conversation = ConversationRepository.findOneByUserIds(userName, targetUserName);
   }
   res.status(200).send(conversation);
 };
