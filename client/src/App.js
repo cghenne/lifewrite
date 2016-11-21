9ff440c8edde63154fee29ebe045101beddd79b0
Tomasz Zubertowski <tomasz.zubertowski@workangel.com>
2016-11-21 10:43:05 +0000

    Adds notifications

--- client/src/App.js
+++ client/src/App.js
@@ -52,8 +52,12 @@ class App extends Component {
         this.state.socket.on('receive:message', data => {
           let {messages} = this.state;
           console.log(data);
-          messages.push(data.message);
-          this.setState({messages: messages});
+          if (localGet('currentConversation').id === data.conversationId) {
+            messages.push(data.message);
+            this.setState({messages: messages});
+          } else {
+            handleUnreadMessage(data, this.state.users)
+          }
         });
 
         this.state.socket.on('update:userlist', data => {
@@ -272,6 +276,25 @@ const findUser = (id, users, alternativeId) => {
     }
   });
   return foundUser;
+}
+
+const handleUnreadMessage = (data, users) => {
+  if (!("Notification" in window)) {
+    console.log('this browser does not support notifications')
+    return
+  }
+  const user = findUser(data.sender, users)
+  console.log(user)
+  let tittle = "Message from: " + user.name
+  if (Notification.permission === "granted") {
+    let notification = new Notification(tittle, {body: data.message.message});
+  } else if (Notification.permission !== 'denied') {
+    Notification.requestPermission(function (permission) {
+      if (permission === "granted") {
+        let notification = new Notification(tittle, {body: data.message});
+      }
+    });
+  }
 }
 
 const customStyles = {
